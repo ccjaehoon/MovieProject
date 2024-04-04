1c0eed23e9c9cda7499180dcf36aa3f5bcf7ec1c
Sejun Choi <chltpwns5801@gmail.com>
2024-04-04 17:04:13 +0900

    new branch

--- movie/src/main/java/com/project/movie/news/NewsDAO.java
+++ movie/src/main/java/com/project/movie/news/NewsDAO.java
@@ -1,5 +1,22 @@
 package com.project.movie.news;
 
-public interface NewsDAO {
+import java.util.List;
 
+public interface NewsDAO {
+	
+	public List<NewsVO> n_selectAll(int cpage, int pageBlock);
+
+	public NewsVO n_selectOne(NewsVO vo);
+
+	public List<NewsVO> n_searchList(String searchKey, String searchWord, int cpage, int pageBlock);
+
+	public int n_insert(NewsVO vo);
+
+	public int n_delete(NewsVO vo);
+
+	public List<NewsVO> n_update(NewsVO vo);
+
+	public int n_getTotalRows();
+
+	public int n_getSearchTotalRows(String searchKey, String searchWord);
 }
--- movie/src/main/java/com/project/movie/news/NewsVO.java
+++ movie/src/main/java/com/project/movie/news/NewsVO.java
@@ -1,5 +1,15 @@
 package com.project.movie.news;
 
-public class NewsVO {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class NewsVO {
+	private int news_num;
+	private String title;
+	private String nickname;
+	private String content;
+	private Date wdate;
+	private int good;
 }
