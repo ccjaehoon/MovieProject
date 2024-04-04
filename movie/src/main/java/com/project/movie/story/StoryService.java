b056c524ba185fcf3c56d4ae54c61740b2574ed9
(Seyoung Choi) <(c91sy@naver.com)>
2024-04-04 18:00:39 +0900

    스토리 서비스, 서비스임플 완

--- movie/src/main/java/com/project/movie/story/StoryService.java
+++ movie/src/main/java/com/project/movie/story/StoryService.java
@@ -1,5 +1,24 @@
 package com.project.movie.story;
 
+import java.util.Date;
+import java.util.List;
+
 public interface StoryService {
+	
+	public int s_insert(StoryVO vo);
+
+	public int s_update(StoryVO vo);
+
+	public int s_delete(StoryVO vo);
+	
+	public List<StoryVO> s_selectOne(StoryVO vo);
+	
+	public List<StoryVO> s_selectAll();
+	
+	public List<StoryVO> s_selectAll(int cpage,int pageBlock);
+	
+	Date s_getLiveStories(StoryVO vo);
+	
+	public int s_removeExpired();
 
 }
