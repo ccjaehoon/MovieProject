811719709e7f9d4b97b5cb491cb645d847094ff0
Hankyu Shin <brad0501@naver.com>
2024-04-04 14:07:28 +0900

    test3

--- movie/src/main/java/com/project/movie/faq/FaqDAO.java
+++ movie/src/main/java/com/project/movie/faq/FaqDAO.java
@@ -1,5 +1,15 @@
 package com.project.movie.faq;
 
+import java.util.List;
+
 public interface FaqDAO {
+	
+	public int f_selectOne(FaqVO vo);
+	
+	public List<FaqVO> f_selectAll(int cpage,int pageBlock);
+	
+	public List<FaqVO> f_searchList(String searchKey, String searchWord,int cpage,int pageBlock);
+
+	public int f_getSearchTotalRows(String searchKey, String searchWord);
 
 }
