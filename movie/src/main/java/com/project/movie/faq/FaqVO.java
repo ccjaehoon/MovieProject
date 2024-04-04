6ba56377d5ce8c06ca23ad97fb7da0762522817b
Hankyu Shin <brad0501@naver.com>
2024-04-04 14:14:40 +0900

    FaqVO update

--- movie/src/main/java/com/project/movie/faq/FaqVO.java
+++ movie/src/main/java/com/project/movie/faq/FaqVO.java
@@ -1,5 +1,10 @@
 package com.project.movie.faq;
 
+@Data
 public class FaqVO {
-
+	private int faq_num;
+	private String title;
+	private String nickname;
+	private String content;
+	private String authority;
 }
