a57d497d68085054e978ad721b07afb9e5c52403
Hankyu Shin <brad0501@naver.com>
2024-04-04 14:36:09 +0900

    controller

--- movie/src/main/java/com/project/movie/news/comments/NCommentsRestController.java
+++ movie/src/main/java/com/project/movie/news/comments/NCommentsRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@RestController
+@Controller
 public class NCommentsRestController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(NCommentsRestController.class);
