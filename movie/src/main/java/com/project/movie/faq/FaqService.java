a2cdae45bafa477f284c9904441ee766a0cae9fe
Hankyu Shin <brad0501@naver.com>
2024-04-04 14:08:58 +0900

    test3

--- movie/src/main/java/com/project/movie/faq/FaqService.java
+++ movie/src/main/java/com/project/movie/faq/FaqService.java
@@ -1,5 +1,14 @@
 package com.project.movie.faq;
 
-public interface FaqService {
+import java.util.List;
 
+public interface FaqService {
+	
+	public int f_selectOne(FaqVO vo);
+	
+	public List<FaqVO> f_selectAll(int cpage,int pageBlock);
+	
+	public List<FaqVO> f_searchList(String searchKey, String searchWord,int cpage,int pageBlock);
+
+	public int f_getSearchTotalRows(String searchKey, String searchWord);
 }
--- movie/src/main/java/com/project/movie/faq/FaqServiceimpl.java
+++ movie/src/main/java/com/project/movie/faq/FaqServiceimpl.java
@@ -1,5 +1,31 @@
 package com.project.movie.faq;
 
+import java.util.List;
+
 public class FaqServiceimpl implements FaqService {
+
+	@Override
+	public int f_selectOne(FaqVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<FaqVO> f_selectAll(int cpage, int pageBlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FaqVO> f_searchList(String searchKey, String searchWord, int cpage, int pageBlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int f_getSearchTotalRows(String searchKey, String searchWord) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
