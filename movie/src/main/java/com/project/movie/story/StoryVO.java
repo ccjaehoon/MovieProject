e4238f48df820e2aa44fbe24d650316d41040688
(Seyoung Choi) <(c91sy@naver.com)>
2024-04-04 17:18:54 +0900

    qqq

--- movie/src/main/java/com/project/movie/story/StoryVO.java
+++ movie/src/main/java/com/project/movie/story/StoryVO.java
@@ -1,5 +1,23 @@
 package com.project.movie.story;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class StoryVO {
+	private int story_num;
+	private String nickname;	
+	private String storycontent;
+	private String savename;
+	private String file;
+	private Date wdate;
+	private int good;
+	private int views;
+	private Date duration; //스토리가 표시되는 시간
+	private String report;
+	
+
+	
 
 }
