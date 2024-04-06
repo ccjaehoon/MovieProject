1c470f9b056c7bc1903d7ba04408f07055e08f2a
Hankyu Shin <brad0501@naver.com>
2024-04-06 15:24:51 +0900

    controller home.do delete

--- movie/src/main/java/com/project/movie/news/comments/NCommentsController.java
+++ movie/src/main/java/com/project/movie/news/comments/NCommentsController.java
@@ -22,19 +22,7 @@ public class NCommentsController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+
 	
 	@RequestMapping(value = "/nc_insert.do", method = RequestMethod.GET)
 	public String nc_insert() {
